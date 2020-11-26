7ba27bd6826c630f2ebaa0c15f65bb50881a4cda
velinovasen <velinovasen@users.noreply.github.com>
2020-11-26 11:20:21 +0200

    skl

---
+++ router.js
@@ -0,0 +1,11 @@
+const routes = {
+    'home': 'home-template',
+}
+
+const router = async (fullPath) => {
+    console.log(fullPath)
+}
+
+function navigate(href) {
+    console.log(href)
+}
---
+++ services.js
@@ -0,0 +1,2 @@
+const apiKey = `AIzaSyAqkQ6mW6rFLYQgS8QPc3slr6oRq4GOh8E`;
+const databaseURL = 'https://shoe-shelf-df245.firebaseio.com/';
